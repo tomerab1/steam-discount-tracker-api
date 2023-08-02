5c3d0c0f331d5164263c9af3cf0f2209c12eca0a
casio15 <tomer1452002@gmail.com>
2023-08-03 02:09:23 +0300

    Added common dir and constants

--- src/persist-game-info/persist.service.ts
+++ src/persist-game-info/persist.service.ts
@@ -6,6 +6,7 @@ import { SteamFetchResponse } from 'src/steam-data-fetcher/payloads/steam-fetch-
 import { GameInfoEntity } from './entities/game-info.entity';
 import { Repository } from 'typeorm';
 import { SteamGameInfo } from 'src/steam-data-fetcher/payloads/steam-app.payload';
+import { EMPTY_STRING } from 'src/common/constants';
 
 @Injectable()
 export class PersistService {
@@ -17,7 +18,7 @@ export class PersistService {
   @OnEvent(EVENT_GAME_INFO_FETCHED, { async: true, promisify: true })
   persistData(payload: SteamFetchResponse) {
     payload.apps.forEach(async (info: SteamGameInfo) => {
-      if (info.name !== '') {
+      if (info.name !== EMPTY_STRING) {
         const gameInfoEntity = await this.gameInfoRepo.create({ ...info });
         await this.gameInfoRepo.save(gameInfoEntity);
       }

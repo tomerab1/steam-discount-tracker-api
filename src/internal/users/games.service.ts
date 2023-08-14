d215ceb6184154650f6e45632b7f713c4da33c3a
CASIO15 <tomer1452002@gmail.com>
2023-08-14 23:44:55 +0300

    Removed redundant function

--- src/internal/users/games.service.ts
+++ src/internal/users/games.service.ts
@@ -19,35 +19,6 @@ export class GamesService {
     return game;
   }
 
-  async update(id: string, updateGameDto: CreateGamesDto) {
-    try {
-      const user = await this.usersService.findOne(id);
-      const createdGames = [];
-
-      const work = updateGameDto.games.map(async (gameName: string) => {
-        const savedGame = await this.findOneByName(gameName);
-        let gameEntity = undefined;
-
-        if (!savedGame) {
-          gameEntity = await this.create(id, { games: [gameName] });
-        } else {
-          gameEntity = await this.gameRepository.preload({
-            id: savedGame.id,
-            name: savedGame.name,
-            users: [...savedGame.users, user],
-          });
-        }
-
-        return createdGames.push(await this.gameRepository.save(gameEntity));
-      });
-
-      await Promise.all(work);
-      return createdGames;
-    } catch (error) {
-      throw error;
-    }
-  }
-
   async create(id: string, createGamesDto: CreateGamesDto) {
     const user = await this.usersService.findOne(id);
     const createdGames = [];
